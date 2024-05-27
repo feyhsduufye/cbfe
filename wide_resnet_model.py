263754a37e4f3fd7a7958e97c9ee2cfe5e6b057b
feyhsduufye <zhengqian202311@163.com>
2024-05-27 09:37:59 +0800

    Update wide_resnet_model.py

--- wide_resnet_model.py
+++ wide_resnet_model.py
@@ -6,7 +6,6 @@ from utils import *
 from fusion import AFF
 
 
-factors
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
